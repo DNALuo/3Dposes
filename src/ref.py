5c5cad0b683a97ce32ef82682a3b15d0866db0c7
DNALuo <breezefanning@gmail.com>
2018-11-12 21:09:23 -0500

    Joint Info

--- src/ref.py
+++ src/ref.py
@@ -1,4 +1,8 @@
 nJoints = 16
+#  0 - r ankle,     1 - r knee,      2 - r hip,    3 - l hip, 
+#  4 - l knee,      5 - l ankle,     6 - pelvis,   7 - thorax, 
+#  8 - upper neck,  9 - head top,   10 - r wrist, 11 - r elbow, 
+# 12 - r shoulder, 13 - l shoulder, 14 - l elbow, 15 - l wrist
 accIdxs = [0, 1, 2, 3, 4, 5, 10, 11, 14, 15]
 shuffleRef = [[0, 5], [1, 4], [2, 3], 
              [10, 15], [11, 14], [12, 13]]
@@ -26,9 +30,9 @@ hmGaussInp = 20
 shiftPX = 50
 disturb = 10
 
-dataDir = '../data'
-mpiiImgDir = '/home/zxy/Datasets/mpii/images/'
-h36mImgDir = '/home/zxy/Datasets/Human3.6M/images/'
 expDir = '../exp'
+dataDir = '../data'
+mpiiImgDir = '/mnt/Data/mpii/images/'
+h36mImgDir = '/mnt/Data/Human3.6M/images/'
 
 nThreads = 4
