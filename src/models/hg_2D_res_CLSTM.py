852e8a7c59300530d815b09a0fa8c4ef4cbb77fb
dan <breezefanning@gmail.com>
2018-11-20 22:27:58 -0500

    structure for 2D model

---
+++ src/models/hg_2D_res_CLSTM.py
@@ -0,0 +1,16 @@
+import layers.Residual as Residual
+import torch.nn as nn
+import ref
+
+class Hourglass2DLSTM(nn.module):
+    def __init__(self, ppppppp):
+        super(Hourglass2DLSTM,self).__init__()
+
+
+
+    def forward(self, x):
+        
+
+
+        
+        return out
